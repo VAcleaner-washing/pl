9b81dbe739a781f79945b70393dc8d6358b1881a
VAcleaner-washing <vacleaner.washing@gmail.com>
2026-08-05 14:56:21 +0300

    v2.3.2-PERFORMANCE-FIX-GITHUB-PAGES

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacleaner-manager-v12";
+const CACHE = "vacleaner-manager-v13";
 const SHELL = ["/admin/bronuvannia", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png", "/apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
