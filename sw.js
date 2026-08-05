66b054fe16c4d302442698be01cf8d52cf219782
VAcleaner-washing <vacleaner.washing@gmail.com>
2026-08-05 16:16:45 +0300

    2.4.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vacleaner-manager-v2420';
+const CACHE='vacleaner-manager-v2430';
 const CORE=['/admin/bronuvannia/','/assets/admin-v240.css?v=2401','/assets/admin-v240.js?v=2401','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
