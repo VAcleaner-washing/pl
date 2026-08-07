8c5eabc34d5beff32a81bdd93cc8e59d977f6780
VAcleaner-washing <vacleaner.washing@gmail.com>
2026-08-07 15:54:15 +0300

    1

--- supabase/migrations/20260806174500_vacleaner_v3_performance.sql
+++
@@ -1,6 +0,0 @@
--- VAcleaner v3.0: focused performance fixes that do not touch VA HOME data models.
-create index if not exists vacleaner_push_subscriptions_user_idx
-  on public.vacleaner_push_subscriptions (user_id);
-
-alter policy "Admins can read admin allowlist" on public.admin_users
-  using (user_id = (select auth.uid()));
